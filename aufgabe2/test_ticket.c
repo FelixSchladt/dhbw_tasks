7fbb6ce27a27d454e28787b73c63a1a233fe49cd
Felix Schladt <felix.schladt@hensoldt-cyber.com>
2022-04-15 17:39:12 +0200

    wip

--- aufgabe2/test_ticket.c
+++ aufgabe2/test_ticket.c
@@ -25,6 +25,29 @@ static void init() {
     }
 }
 
+/* Returns a random line (w/o newline) from the file provided */
+char* get_random_mail() {
+    FILE *f;
+    size_t lineno = 0;
+    size_t selectlen;
+    char selected[256]; /* Arbitrary, make it whatever size makes sense */
+    char current[256];
+    selected[0] = '\0'; /* Don't crash if file is empty */
+
+    f = fopen("./randommail.txt", "r"); /* Add your own error checking */
+    while (fgets(current, sizeof(current), f)) {
+        if (drand48() < 1.0 / ++lineno) {
+            strcpy(selected, current);
+        }
+    }
+    fclose(f);
+    selectlen = strlen(selected);
+    if (selectlen > 0 && selected[selectlen-1] == '\n') {
+        selected[selectlen-1] = '\0';
+    }
+    return strdup(selected);
+}
+
 char* get_time( char * time  ) {
     sprintf(time, "%02d:%02d", get_random_hour(), get_random_minute());
     return time;
@@ -41,8 +64,9 @@ void get_buchung( char * buchung )  {
 }
 
 
-/*int main(int argc, const char ** argv) {
-    char buchung[256];
-    get_buchung(buchung);
+int main(int argc, const char ** argv) {
+    //char buchung[256];
+    //get_buchung(buchung);
+    printf("%s\n", get_random_mail());
     return 0;
-}*/
+}
