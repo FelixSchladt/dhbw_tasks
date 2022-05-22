0b1dd56c5a0114913dd2e7b2742c4d5483f34683
FelixSchladt <felix.schladt@hensoldt-cyber.com>
2022-05-22 15:15:04 +0200

    fixes clear buffer error

--- aufgabe3/morse_to_text/main.c
+++ aufgabe3/morse_to_text/main.c
@@ -12,20 +12,14 @@
 
 Arguments args;
 
-void init_char_array(char* array, int size) {
-	for (int i = i; i < size; i++) {
-		array[i] = '\0';
-		array++;
-	}
-}
 
 char* translate_text(FILE* file) {
 	char* result       = malloc(MAX_MESSAGE_SIZE);
 	char* letter       = malloc(MAX_LETTER_SIZE);
 	char* first_symbol = letter;
 
-	init_char_array(result, MAX_MESSAGE_SIZE);
-	init_char_array(letter, MAX_LETTER_SIZE);
+	memset(result, '\0', MAX_MESSAGE_SIZE);
+	memset(letter, '\0', MAX_LETTER_SIZE);
 
 	int letter_index = 0;
 	int result_index = 0;
@@ -36,13 +30,13 @@ char* translate_text(FILE* file) {
 			result[result_index] = morse_to_text(letter);
 			result_index++;
 			letter_index = 0;
-			init_char_array(letter, MAX_LETTER_SIZE);
+            memset(letter, '\0', MAX_LETTER_SIZE);
 		} else {
 			letter[letter_index] = symbol;
 			letter_index++;
 		}
 	}
-    printf("letter %c morse_to_text %c",letter,  morse_to_text(letter));
+    //printf("letter %c morse_to_text %c",letter,  morse_to_text(letter));
     
     //TODO BUG????
 	result[result_index] = morse_to_text(letter);
@@ -56,6 +50,7 @@ int main( int argc, char** argv ) {
 	arg_parser(argc, argv, &args);
 	char* translation = translate_text(args.file_pointer);
 	printf("TEXT: %s\n", translation);
+    //printf("TEST: %c")
 
 	free(translation);
 }
