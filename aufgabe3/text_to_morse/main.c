9cd5000bcdb1528b210206485362896a03bf2ad3
FelixSchladt <felix.schladt@hensoldt-cyber.com>
2022-05-22 15:08:13 +0200

    wip

--- aufgabe3/text_to_morse/main.c
+++ aufgabe3/text_to_morse/main.c
@@ -1,3 +1,5 @@
+// Copyright 2022 Felix Schladt (https://github.com/FelixSchladt)
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
--- aufgabe3/text_to_morse/optarg.h
+++ aufgabe3/text_to_morse/optarg.h
@@ -1,3 +1,5 @@
+//Copyright 2022 Felix Schladt (https://github.com/FelixSchladt)
+
 #include <stdbool.h>
 #include <stdio.h>
 
